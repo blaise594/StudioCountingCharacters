5ea723bbbcf70a7bc81865e45b9cb2acdc2d81c2
Daniel <danielbrogers594@gmail.com>
2018-02-08 20:13:29 -0500

    added user input

--- StudioCountingCharacters/Program.cs
+++ StudioCountingCharacters/Program.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace StudioCountingCharacters
+namespace ConsoleApp6
 {
     class Program
     {
         static void Main(string[] args)
         {
             Dictionary<char, int> Counts = new Dictionary<char, int>();
-            String babel= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            Console.WriteLine("Please enter a phrase");
+            string babel = Console.ReadLine();
             char[] characters = babel.ToCharArray();
-
-            foreach(char character in characters)
+            foreach (char character in characters)
             {
-                if(Counts.ContainsKey(character))
+                if (Counts.ContainsKey(character))
                 {
                     Counts[character] = Counts[character] + 1;
                 }
@@ -22,12 +21,14 @@ namespace StudioCountingCharacters
                 {
                     Counts.Add(character, 1);
                 }
-                foreach(KeyValuePair<char, int> Count in Counts)
-                {
-                    Console.WriteLine("There are " + Count.Value+ " of "+ Count.Key);
-                }
-                Console.ReadLine();
+
             }
+            foreach (KeyValuePair<char, int> Count in Counts)
+            {
+                Console.WriteLine("There are " + Count.Value + " of " + Count.Key);
+            }
+            Console.ReadLine();
         }
+
     }
 }
